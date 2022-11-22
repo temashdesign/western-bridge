9de2a280c9e2ef315e9176999f803d043811da44
Artem Shashkin <temash@gmail.com>
2022-11-22 21:05:26 +0100

    page header

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -4,14 +4,19 @@ import pageHeaderBg from '@/images/page-header-bg.png'
 
 export function PageHeader({ className, ...props }) {
   return (
-    <section className={clsx('pahe-header w-full', className)} {...props}>
-      <div>
+    <section className={clsx('page-header w-full', className)} {...props}>
+      <div className="relative grid min-h-[800px] items-center">
+        <h1 className=" relative z-10 mx-auto max-w-7xl -translate-y-6 text-center text-[100px] font-extrabold leading-tight text-white ">
+          <span className="bg-gradient-to-r from-[#1CF6D4] to-blue-500 bg-clip-text text-transparent">
+            We staff your business
+          </span>{' '}
+          on 5 continents of the
+          <br /> world
+        </h1>
         <Image
           src={pageHeaderBg}
-          // width={500}
-          // height={500}
           alt="Picture of the author"
-          className="w-full object-cover"
+          className="absolute inset-0 z-0  h-full w-full object-cover object-bottom"
         />
       </div>
     </section>
